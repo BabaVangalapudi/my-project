e4759ecca6818be5d7a729db8b7934a217dffee2
BabaVangalapudi <babavangalapudi72@gmail.com>
2024-03-07 11:40:39 +0530

    Badminton project

---
+++ src/components/carousel.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+const Carousel = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay:true,
+    autoplaySpeed:4000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  };
+  return (
+    <div>
+      <h2>React Slick Carousel</h2>
+      <Slider {...settings}>
+        <div>
+          <h3>Slide 1</h3>
+        </div>
+        <div>
+          <h3>Slide 2</h3>
+        </div>
+        <div>
+          <h3>Slide 3</h3>
+        </div>
+        {/* Add more slides as needed */}
+      </Slider>
+    </div>
+  );
+};
+export default Carousel;
+
+
+
+
+
+
+
+
+
